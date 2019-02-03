cc8fbcbf0ac750bda9fb0a06f965a814e6f399aa
Bastien0 <bastien.rolland@eleves.enpc.fr>
2019-02-03 21:12:15 +0100

    add comments

--- C++/cplexsolvings.h
+++ C++/cplexsolvings.h
@@ -2,13 +2,19 @@
 #include "path.h"
 #include "infosolution.h"
 
+// Resolution du problème par dualisation du probleme robuste
 InfoSolution dualization(const Instance& instance,  int TimeLim);
+// Meme algorithme avec en entrée une solution réalisable dans path
 InfoSolution dualization(const Instance& instance,  int TimeLim, const Path& path);
 
+// Résolution du problème par plans coupants
 InfoSolution cuttingplanes(const Instance& instance,  int TimeLim);
+// Meme algorithme avec en entrée une solution réalisable dans path
 InfoSolution cuttingplanes(const Instance& instance,  int TimeLim, const Path& path);
 
+// Résolution du problème par branch and cut
 InfoSolution branchandcut(const Instance& instance,  int TimeLim);
+// Meme algorithme avec en entrée une solution réalisable dans path
 InfoSolution branchandcut(const Instance& instance,  int TimeLim, const Path& path);
 
 double get_time();
